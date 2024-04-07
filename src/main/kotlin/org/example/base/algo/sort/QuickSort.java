d352f78ce409259c5ebcf0270b3a1a52245cb0ef
熊盛金 <shengjin.xiong@trustway.cn>
2024-04-07 10:10:33 +0800

    quick sort

--- src/main/kotlin/org/example/base/algo/sort/QuickSort.java
+++ src/main/kotlin/org/example/base/algo/sort/QuickSort.java
@@ -13,32 +13,48 @@ public class QuickSort {
         var arr = new int[]{3, 2, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
+        var nums = new int[]{3, 2, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        System.out.println(findKth(nums, 5));
     }
 
-    public static void quickSort(int[] arr, int left, int right) {
-        var pivot = left + (right - left) / 2;
-        var pivotValue = arr[pivot];
+    public static void quickSort(int[] num, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = partition(num, left, right);
+        quickSort(num, left, pivot - 1);
+        quickSort(num, pivot + 1, right);
+    }
+
+    private static int partition(int[] nums, int left, int right) {
+        var median = median(nums, left, left + (right - left) / 2, right);
+        swap(nums, left, median);
+        int pivot = nums[left];
+
         var i = left;
         var j = right;
-        while (i <= j) {
-            while (arr[i] < pivotValue) {
-                i++;
-            }
-            while (arr[j] > pivotValue) {
+        while (i < j) {
+            while (i < j && nums[j] >= pivot) {
                 j--;
             }
-            if (i <= j) {
-                swap(arr, i, j);
+            while (i < j && nums[i] <= pivot) {
                 i++;
-                j--;
             }
-            if (left < j) {
-                quickSort(arr, left, j);
-            }
-            if (i < right) {
-                quickSort(arr, i, right);
-            }
+            swap(nums, i, j);
         }
+        swap(nums, left, i);
+        return i;
+    }
+
+    // 三个数的中位数
+    private static int median(int[] nums, int left, int mid, int right) {
+        if ((nums[left] < nums[right]) ^ (nums[left] < nums[mid])) {
+            return left;
+        }
+        if ((nums[mid] < nums[left]) ^ (nums[mid] < nums[right])) {
+            return mid;
+        }
+        return right;
     }
 
     private static void swap(int[] arr, int i, int j) {
@@ -48,6 +64,29 @@ public class QuickSort {
         var temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    // 快速排序查找第 k 大的数
+    public static int findKth(int[] nums, int k) {
+        if (nums.length < k) {
+            return -1;
+        }
+        int target = nums.length - k;
+        int start = 0;
+        int end = nums.length - 1;
+        int index = partition(nums, start, end);
+        // 第 k 大的数下标是 len - k
+        while (index != target) {
+            if (index < target) {
+                start = index + 1;
+            } else {
+                end = index - 1;
+            }
+
+            index = partition(nums, start, end);
+        }
+
+        return nums[index];
     }
 }
 
